f38db620f8a5a12e1d6199f84bc99e6d596527aa
J. Muzina <jmuzina@pm.me>
2022-01-18 20:43:50 -0500

    put params in url

--- api/index.ts
+++ api/index.ts
@@ -6,6 +6,23 @@ import { playerDetails } from "../data/PlayerDetails";
 let pitchList = {};
 let playerList = {};
 
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
+
 for (const index in pitches) {
     const pid = pitches[index].pitcherId.toString();
     if (pitchList[pid] == null) {
@@ -35,8 +52,25 @@ const endpoints = {
     }
 }
 
-export default (request: VercelRequest, response: VercelResponse) => {
-    const callType = request.headers.calltype;
+module.exports = (request: VercelRequest, response: VercelResponse) => {
+    let charPos = request.url.indexOf('?') + 1;
+    const paramsStr = request.url.substring(charPos, request.url.length);
+    charPos = 0;
+    let params = {};
+
+    while (charPos < paramsStr.length) {
+        let endOfParamPos = paramsStr.indexOf('&', charPos);
+        if (endOfParamPos == -1)
+            endOfParamPos = paramsStr.length;
+
+        let endOfKeyPos = paramsStr.indexOf('=', charPos);
+        let key = paramsStr.substring(charPos, endOfKeyPos);
+        let val = paramsStr.substring(endOfKeyPos + 1, endOfParamPos)
+        params[key] = val;
+        charPos = endOfParamPos + 1;
+    }
+
+    const callType = params['calltype'];
     if (!endpoints[callType] || !callType)
         return response.json({ error: "Bad or invalid call type specified in req header.. Valid types: " + (()=>{
             let res = "";
@@ -52,5 +86,5 @@ export default (request: VercelRequest, response: VercelResponse) => {
         'Access-Control-Allow-Headers',
         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     );
-    response.status(200).send(endpoints[callType](request.headers));
+    response.status(200).send(endpoints[callType](params));
 };
